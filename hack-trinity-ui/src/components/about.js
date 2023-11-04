5130ebeb84d7f6a78f2b0d98e47147666ea94158
jathonderulo <119631105+jathonderulo@users.noreply.github.com>
2023-11-04 14:50:25 +0000

    added white bar, added svg

--- hack-trinity-ui/src/components/about.js
+++ hack-trinity-ui/src/components/about.js
@@ -1,9 +1,13 @@
+import knowledge from "../media/knowledge.svg";
 
 function about() {
     return (
-        <div className="bg-secondary about-section">
-            <div className="bg-white about-text">
-                
+        <div className="bg-secondary about-section flex justify-between">
+            <img src={knowledge} className="knowledge relative"></img>
+            <span className="bg-white white-bar relative rounded-lg"></span>
+            <div className="border border-black border-solid about-text relative right-8">
+                <h2 className="text-white text-center pt-5"> What is Trinity Exam Search? </h2>
+                <span className="bg-white title-bar relative rounded-lg">?</span>
             </div>
         </div>
   
