c78b7d04490603fe4dab12c9964ef1cc3c9f2a68
jathonderulo <119631105+jathonderulo@users.noreply.github.com>
2023-11-04 15:04:10 +0000

    about section complete for now

--- hack-trinity-ui/src/components/about.js
+++ hack-trinity-ui/src/components/about.js
@@ -5,9 +5,22 @@ function about() {
         <div className="bg-secondary about-section flex justify-between">
             <img src={knowledge} className="knowledge relative"></img>
             <span className="bg-white white-bar relative rounded-lg"></span>
-            <div className="border border-black border-solid about-text-box relative right-8">
+            <div className="border-2 border-white border-solid about-text-box relative right-8">
                 <h2 className="text-white text-center pt-5"> What is Trinity Exam Search? </h2>
-                <p className="about-paragraph relative">This is what it is "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                <p className="about-paragraph relative">
+                    Do you ever think to yourself 'It is so easy to find past exams for my modules'?
+                    No. But that's because you haven't used Trinity Exam Search before. We CS students single handedly
+                    improved Blackboard.
+
+                    <br/><br/>
+                    Type in your stuff and we find ALL your past exams that are available across Trinity's web presence. 
+                    If it's not here, it doesn't exist. We also don't take responsibility for anything bad.
+                    <br/><br/>
+
+                    Made by students, for students.     
+                    Better Blackboard ™
+
+                </p>
                 <span className="bg-white title-bar absolute rounded-lg"></span>
             </div>
         </div>
