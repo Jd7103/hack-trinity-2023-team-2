3a83ce858ca5d65774a3a8e9ee359f6d980b1d6e
James Doyle <doylej46@tcd.ie>
2023-11-04 16:58:18 +0000

    New API call for retrieving a PDF given a URL.

--- blackboard_but_better/src/main/java/APICalls.java
+++ blackboard_but_better/src/main/java/APICalls.java
@@ -1,3 +1,8 @@
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+
+import java.util.Base64;
+
 import static spark.Spark.*;
 
 public class APICalls {
@@ -14,6 +19,23 @@ public class APICalls {
                     req.queryParams("pass"));
 
             return crawler.init();
+        });
+
+        get("/getPDF", (req, res)->{
+            String url = req.queryParams("url");
+
+            String user = req.queryParams("user");
+            String pass = req.queryParams("pass");
+
+            String auth = user + ":" + pass;
+            auth = Base64.getEncoder().encodeToString(auth.getBytes());
+
+            return Jsoup.connect(url)
+                    .ignoreContentType(true)
+                    .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36")
+                    .header("authorization", auth)
+                    .method(Connection.Method.GET)
+                    .execute();
         });
 
     }
