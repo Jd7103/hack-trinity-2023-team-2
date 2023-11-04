86a51dcdf6216388a8e3c197a1ed389f75badb1d
jathonderulo <119631105+jathonderulo@users.noreply.github.com>
2023-11-04 14:27:02 +0000

    added about section

---
+++ hack-trinity-ui/src/components/about.js
@@ -0,0 +1,14 @@
+
+function about() {
+    return (
+        <div className="bg-secondary about-section">
+            <div className="bg-white about-text">
+                
+            </div>
+        </div>
+  
+    )
+  }
+
+
+export default about;
