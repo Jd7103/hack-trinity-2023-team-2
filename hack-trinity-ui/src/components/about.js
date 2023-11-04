edf96b45c52728a43a191c577bb8285601cf250a
jathonderulo <119631105+jathonderulo@users.noreply.github.com>
2023-11-04 14:54:32 +0000

    about text box title bar done, paragraph done.

--- hack-trinity-ui/src/components/about.js
+++ hack-trinity-ui/src/components/about.js
@@ -5,9 +5,10 @@ function about() {
         <div className="bg-secondary about-section flex justify-between">
             <img src={knowledge} className="knowledge relative"></img>
             <span className="bg-white white-bar relative rounded-lg"></span>
-            <div className="border border-black border-solid about-text relative right-8">
+            <div className="border border-black border-solid about-text-box relative right-8">
                 <h2 className="text-white text-center pt-5"> What is Trinity Exam Search? </h2>
-                <span className="bg-white title-bar relative rounded-lg">?</span>
+                <p className="about-paragraph relative">This is what it is "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                <span className="bg-white title-bar absolute rounded-lg"></span>
             </div>
         </div>
   
